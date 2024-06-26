227647a9387582e903d5a21bbc9c466b026bd10a
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2024-06-26 16:08:24 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 372 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
@@ -4,29 +4,32 @@ input = sys.stdin.readline
 
 """
 # 가장 가까운 세 사람의 심리적 거리 (20529)
- 1. MBTI에서 다른 알파벳 하나당 심리적 거리 +1
- 2. 세 사람의 심리적 거리
-  = (A & B) + (B & C) + (C & A)
+ 1. 세 사람의 심리적 거리 구하기
+ 2. 두 사람 사이의 유형이 다르면 심리적인 거리 +
+ 3. 세 사람의 심리적인 거리 = AB 거리 + BC 거리 + CA 거리
 [입력]
  1. T: 테스트 케이스 수
  2. N: 학생의 수
+ 3. 두번째 줄: 각 학생의 MBTI
 [출력]
- 1. 각 테스트 케이스에 대한 답을 한 줄에 하나씩 출력
+ 1. 가장 가까운 세 학생 사이의 심리적인 거리 구하기
 """
 
 """
-<풀이>
- 1. 비둘기집 원리
+<풀이> 
+ 1. 이미 푼 문제
+ 2. 비둘기집 원리
   - 비둘기가 n마리, 비둘기 집이 m개 있을 때,
   - n이 m보다 크면 적어도 하나의 집에는 비둘기가 2마리 이상 들어간다는 원리
-  - 16 x 3이 될 경우 적어도 3명은 겹치게 됨 -> 심리적 거리 0
+  - 16 x 2 + 1이 될 경우 적어도 3명은 겹치게 됨 -> 심리적 거리 0
 """
 
 
-# 심리적 거리 구하기
-def cal(A, B, C):
+# 세 사람 사이의 거리 구하기
+def distant(A, B ,C):
     dist = 0
     for i in range(4):
+        # 세 명과 비교
         if A[i] != B[i]:
             dist += 1
         if B[i] != C[i]:
@@ -40,22 +43,21 @@ def cal(A, B, C):
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
-    students = list(map(str, input().split()))
 
-    # 비둘기집 원리
+    mbti = list(map(str, input().split()))
+
+    # 학생 수가 32 초과일 경우 -> 적어도 3명은 겹쳐짐 = 심리적 거리 0
     if N > 32:
         print(0)
 
     else:
-        # 최댓값
+        # 세 사람의 심리적 거리 최대 값 = 12 (AB = 4, BC = 4, CA = 4)
         answer = 12
 
-        # 세 수 순회
-        for A in range(N):
-            for B in range(A + 1, N):
+        # 순회
+        for A in range(N - 2):
+            for B in range(A + 1, N - 1):
                 for C in range(B + 1, N):
-                    answer = min(answer, cal(students[A], students[B], students[C]))
+                    answer = min(answer, distant(mbti[A], mbti[B], mbti[C]))
 
         print(answer)
-
-
