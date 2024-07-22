95b1aebe736a23d68ba3c6c13681e0e0f5fddc4a
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2024-07-22 22:07:00 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,22 @@
+import sys
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+
+def dynamic_programming(n):
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    if n == 2:
+        dp[2] = 2
+    elif n >= 3:
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+
+    return dp[n]
+
+
+n = int(input())
+
+print(dynamic_programming(n))
+
