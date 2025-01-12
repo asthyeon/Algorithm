6d5d6217e31ac515a309d05802f9a14bbcc21bc1
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-01-12 23:28:57 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,15 @@
+import sys
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+divisors = []
+for i in range(1, N + 1):
+    if N % i == 0:
+        divisors.append(i)
+
+if len(divisors) < K:
+    print(0)
+else:
+    print(divisors[K - 1])
