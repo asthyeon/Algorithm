6c5fa5f28fed06aad8b6982d59929469106d8f73
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-01-22 23:38:07 +0900

    [Bronze III] Title: 지능형 기차, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.py
@@ -0,0 +1,14 @@
+import sys
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+answer = 0
+people = 0
+for _ in range(4):
+    drop, ride = map(int, input().split())
+
+    people += ride
+    people -= drop
+    answer = max(answer, people)
+
+print(answer)
