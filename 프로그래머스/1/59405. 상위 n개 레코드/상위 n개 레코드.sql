d858b3b68b93c59d26dba57eb78bdeedd49dc743
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-10-20 17:22:57 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,5 +1,9 @@
--- 코드를 입력하세요
-SELECT NAME
-FROM ANIMAL_INS
-ORDER BY DATETIME
-LIMIT 1;
+-- 날짜에 대한 오름차순으로 정리한후 1개만 출력 -> LIMIT 이용
+SELECT
+    NAME
+FROM 
+    ANIMAL_INS
+ORDER BY
+    DATETIME ASC
+LIMIT
+    1;
