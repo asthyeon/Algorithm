2bd9103c5e6c8273ca62804e5c7b597a366b8849
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-10-20 17:31:35 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,4 +1,18 @@
--- 날짜에 대한 오름차순으로 정리한후 1개만 출력 -> LIMIT 이용
+-- 방법2) 서브쿼리 이용해보기 -> DATETIME에 대한 최소 값 이용
+SELECT
+    NAME
+FROM
+    ANIMAL_INS
+WHERE
+    DATETIME = (
+        SELECT
+            MIN(DATETIME)
+        FROM
+            ANIMAL_INS
+    );
+
+-- 방법1) 날짜에 대한 오름차순으로 정리한후 1개만 출력 -> LIMIT 이용
+/* 
 SELECT
     NAME
 FROM 
@@ -6,4 +20,5 @@ FROM
 ORDER BY
     DATETIME ASC
 LIMIT
-    1;
+    1; 
+*/
