d2ec3c598b3913d24f2741887c34724eff12fbbd
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-10-25 18:22:15 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,13 +1,16 @@
--- 코드를 작성해주세요
-# 각 대장균별 형질 숫자를 2진수로 바꾸기
-# 뒤에서부터 1번 순서
+-- 개체 형질을 2진수로 변환 -> 자리수가 1인경우 형질 보유 
+-- 2진수로 변환하기 위해 CONV(넣을 숫자, 넣을 숫자의 진법, 변환할 진법) or BIN(넣을 숫자) 사용
 SELECT
     COUNT(ID) AS COUNT
 FROM
     ECOLI_DATA
 WHERE
+    -- 2진법으로 전환했을 때 역순으로 형질 계산
+    -- 2번 형질 미보유
     BIN(GENOTYPE) NOT LIKE '%1_'
     AND (
-        BIN(GENOTYPE) LIKE '%1'
-        OR BIN(GENOTYPE) LIKE '%1__'
+        -- 1번 형질 보유
+        CONV(GENOTYPE, 10, 2) LIKE '%1'
+        -- 3번 형질 보유
+        OR CONV(GENOTYPE, 10, 2) LIKE '%1__'
     );
