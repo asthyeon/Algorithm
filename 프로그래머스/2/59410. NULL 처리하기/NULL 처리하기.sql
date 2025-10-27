507f00e59adc8d07cd8234797fc686a9c28e287b
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-10-27 23:24:17 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,7 +1,7 @@
--- 코드를 입력하세요
+-- COALESCE 사용 -> COALESCE(넣을 값1, 넣을 값2, 넣을 값3, ...) 처음으로 NULL이 아닌 값 표시
 SELECT
     ANIMAL_TYPE,
-    IFNULL(NAME, "No name") AS NAME,
+    COALESCE(NAME, 'No name') AS NAME,
     SEX_UPON_INTAKE
 FROM
     ANIMAL_INS
