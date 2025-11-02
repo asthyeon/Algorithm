4b970a66bf5ce88bc3c9a560183606a19795b5c0
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-11-02 23:38:06 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,4 +1,5 @@
--- 코드를 입력하세요
+-- CASE로 여러 조건 나누기
+-- 실제 날짜 형식이 예시와 다름 -> LIKE로 찾기
 SELECT
     BOARD_ID,
     WRITER_ID,
@@ -8,10 +9,10 @@ SELECT
         WHEN STATUS = 'SALE' THEN '판매중'
         WHEN STATUS = 'RESERVED' THEN '예약중'
         ELSE '거래완료'
-    END AS "STATUS"
+    END AS STATUS
 FROM
     USED_GOODS_BOARD
 WHERE
-    CREATED_DATE = '2022-10-05'
+    CREATED_DATE LIKE '2022-10-05%'
 ORDER BY
     BOARD_ID DESC;
