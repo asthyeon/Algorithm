eef3f613bee6081cd76a3766fbea2413c6955f29
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-11-03 22:42:57 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,20 +1,22 @@
--- 코드를 작성해주세요
+-- 3개의 테이블 JOIN
+-- 상, 하반기의 점수의 합을 구해야 함 -> 사번으로 GROUP BY
+-- 내림차순으로 정렬 후, 상위 1개만 출력
+
 SELECT
-    SUM(HG.SCORE) AS SCORE,
+    SUM(SCORE) AS SCORE,
     HE.EMP_NO,
     HE.EMP_NAME,
     HE.POSITION,
     HE.EMAIL
 FROM
     HR_DEPARTMENT HD
-    LEFT JOIN HR_EMPLOYEES HE
-    ON HD.DEPT_ID = HE.DEPT_ID
-    LEFT JOIN HR_GRADE HG
-    ON HE.EMP_NO = HG.EMP_NO
-WHERE
-    HG.YEAR = '2022'
+    JOIN HR_EMPLOYEES HE
+        ON HD.DEPT_ID = HE.DEPT_ID
+    JOIN HR_GRADE HG
+        ON HE.EMP_NO = HG.EMP_NO
 GROUP BY
     HE.EMP_NO
 ORDER BY
     SCORE DESC
-LIMIT 1;
+LIMIT
+    1;
