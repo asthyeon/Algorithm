60742bd15957f0e2374f9cd3dc1b9f05d47d8eec
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-11-04 22:12:45 +0900

    [level 2] Title: 노선별 평균 역 사이 거리 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284531. 노선별 평균 역 사이 거리 조회하기/노선별 평균 역 사이 거리 조회하기.sql
+++ 프로그래머스/2/284531. 노선별 평균 역 사이 거리 조회하기/노선별 평균 역 사이 거리 조회하기.sql
@@ -1,4 +1,9 @@
--- 코드를 작성해주세요
+-- 총 누계거리: 역 사이 거리의 총합
+-- GROUP BY의 경우 SELECT에서 '*'을 쓸 경우, 그룹화된 컬럼의 수가 더 적을 경우 오류 발생
+-- CONCAT으로 'km' 붙이기
+-- ROUND 함수 -> 정수를 넣을 시 해당 자리까지 나타내는 것
+-- 정렬 기준 -> km가 포함된 문자열 X, 총 누계거리의 숫자값으로 
+
 SELECT
     ROUTE,
     CONCAT(ROUND(SUM(D_BETWEEN_DIST), 1), 'km') AS TOTAL_DISTANCE,
@@ -8,4 +13,5 @@ FROM
 GROUP BY
     ROUTE
 ORDER BY
-    ROUND(SUM(D_BETWEEN_DIST), 1) DESC;
+    -- 정렬을 문자열이 아닌 숫자로 해야함
+    SUM(D_BETWEEN_DIST) DESC;
