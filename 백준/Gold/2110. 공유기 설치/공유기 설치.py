a754c732c5e85627e826301ae8c8f6bd09ee8311
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2025-12-29 01:37:49 +0900

    [Gold IV] Title: 공유기 설치, Time: 568 ms, Memory: 40140 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -3,62 +3,51 @@ import sys
 input = sys.stdin.readline
 
 """
-# C 개의 공유기를 N 개의 집에 적당히 설치 후 가장 인접한 두 공유기 사이의 거리를 최대로 구하기
-1. 한 집에는 공유기 하나만 설치 가능
+[문제] 공유기 설치 (2110)
+1. 최대한 많은 곳에서 와이파이를 설치
 2. 가장 인접한 두 공유기 사이의 거리를 가능한 크게 하여 설치
-@ 풀이
-(1) 이분 탐색으로 거리의 간격을 찾기
-(2) mid를 거리의 간격으로 놓기
-(3) 최대 거리를 찾아야 하므로 첫집과 마지막집은 무조건 선택
+
+[풀이]
+1. 이진 탐색
+2. 설치 갯수가 크거나 같을 때도 정답 갱신하기
 """
 
 
-# 이분탐색 함수
-def binary_search(houses, N, C):
-    # 시작점은 1(간격이 0이 될 수 없기 때문)
+def binary_search(N, C, houses):
     start = 1
-    # 끝점은 마지막집 위치에서 첫집의 위치를 빼주기
-    end = houses[-1] - houses[0]
-    result = 0
+    end = houses[-1]
+    answer = 0
+
     while start <= end:
-        # 설치될 거리의 간격
-        mid = (start + end) // 2
-        # 공유기가 설치된 집의 수(첫집은 무조건 설치)
-        cnt = 1
-        # 현재 공유기가 설치된 위치
-        install = houses[0]
+        middle = (start + end) // 2
 
-        # 공유기 설치(첫집은 생략)
-        for i in range(1, N):
-            # 다음 집이 현재 설치된 곳부터 간격만큼 떨어졌을 때
-            if houses[i] >= install + mid:
-                # 설치 추가 및 현재 설치된 위치 갱신
-                cnt += 1
-                install = houses[i]
+        # 공유기 설치 수, 설치된 위치
+        install = 0
+        installed = 0
+        for i in range(N):
+            # 처음에는 무조건 설치
+            if i == 0:
+                install += 1
+                installed = houses[i]
+                continue
+            # 현재 위치에 설치할 수 있는 경우 설치
+            if abs(houses[i] - installed) >= middle:
+                install += 1
+                installed = houses[i]
 
-        # 설치된 공유기가 설치할 수 있는 공유기보다 많거나 같다면 간격 늘리기
-        if cnt >= C:
-            start = mid + 1
-            # C 만큼 설치된 상황에서의 최대 간격 교체
-            result = mid
-        # 설치된 공유기가 설치할 수 있는 공유기보다 적다면 간격 좁히기
+        # 더 많이 설치할 수 있거나 같으면 정답 갱신 및 간격 늘리기
+        if install >= C:
+            start = middle + 1
+            answer = middle
+        # 적게 설치되면 간격 줄이기
         else:
-            end = mid - 1
+            end = middle - 1
 
-    return result
+    return answer
 
 
-# 집 수 N, 공유기 수 C
 N, C = map(int, input().split())
-
-# 집들의 위치 받기
-houses = []
-for _ in range(N):
-    loc = int(input())
-    houses.append(loc)
-
-# 이분탐색을 위한 정렬
+houses = [int(input()) for _ in range(N)]
 houses.sort()
 
-# 함수 사용
-print(binary_search(houses, N, C))
+print(binary_search(N, C, houses))
