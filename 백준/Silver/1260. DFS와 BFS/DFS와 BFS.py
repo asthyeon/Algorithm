499841450a90d19371ec67f4fd304c1c3eaffcd2
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-01-05 21:37:47 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35580 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -3,75 +3,71 @@ import sys
 input = sys.stdin.readline
 
 """
-<문제>
-# DFS와 BFS
-1. DFS로 탐색한 결과, BFS로 탐색한 결과 출력
- - 정점 번호가 작은 것을 먼저 방문
- - 더 이상 방문할 수 있는 점이 없는 경우 종료
- - 양방향 그래프
-<풀이>
-1. DFS, BFS
+[문제] DFS와 BFS (1260)
+1. DFS로 탐색한 결과와 BFS로 탐색한 결과 출력
+2. 정점 번호가 작은 것을 먼저 방문, 더 이상 방문 불가능한 경우 종료
+
+[풀이]
+1. DFS/BFS
 """
 from collections import deque
 
 
 def dfs(graph, V):
-    # 정점 순서 정렬(역방향) -> 정방향으로 탐색이 되게끔
-    for i in range(1, N + 1):
-        graph[i].sort(reverse=True)
-
-    # 변수 및 시작점 설정
+    # 방문 리스트 및 시작점 방문 설정
     visited = [0] * (N + 1)
-    answer = []
-    stack = [V]
+    q = [V]
+    visited[V] = 1
+    answer = [V]
 
-    while stack:
-        # 현재 방문 노드 처리
-        now = stack.pop()
-        if not visited[now]:
-            answer.append(now)
-            visited[now] = 1
+    while q:
+        # 현재 위치
+        now = q[-1]
 
-            # 다음 방문 노드 처리
-            for new in graph[now]:
-                if not visited[new]:
-                    stack.append(new)
+        for new in graph[now]:
+            # 방문하지 않은 곳이라면 이동
+            if not visited[new]:
+                visited[new] = 1
+                q.append(new)
+                answer.append(new)
+                break
+        # 현재 단계에서 다 방문했다면 이전으로 이동
+        else:
+            if q:
+                q.pop()
 
     return answer
 
 
 def bfs(graph, V):
-    # 정점 순서 정렬(정방향)
-    for i in range(1, N + 1):
-        graph[i].sort()
-
-    # 변수 및 시작점 설정
+    # 방문 리스트 및 시작점 방문 설정
     visited = [0] * (N + 1)
     q = deque([V])
-    answer = []
     visited[V] = 1
+    answer = [V]
 
     while q:
-        # 현재 방문 노드 처리
         now = q.popleft()
-        answer.append(now)
 
-        # 다음 방문 노드 처리
         for new in graph[now]:
+            # 방문하지 않은 곳이라면 방문
             if not visited[new]:
                 visited[new] = 1
                 q.append(new)
+                answer.append(new)
 
     return answer
 
 
-# 정점 수 N, 간선 수 M, 시작점 V
 N, M, V = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
 for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+# 오름차순 정렬
+for g in graph:
+    g.sort()
 
 print(*dfs(graph, V))
 print(*bfs(graph, V))
