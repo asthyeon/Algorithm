a5681cbfe8711f38fd0be7d2044c6130b23c573c
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-01-06 01:52:52 +0900

    [Gold V] Title: MooTube (Silver), Time: 2944 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -10,50 +10,42 @@ input = sys.stdin.readline
 
 [풀이]
 1. bfs
+2. 가중치가 K 이상인 간선만 이동해보기
 """
 from collections import deque
 
 
-def bfs(K, start, graph, maximum):
+def bfs(K, start, graph):
     visited = [0] * (N + 1)
-    q = deque([(maximum, start)])
+    q = deque([start])
     visited[start] = 1
 
-    while q:
-        now_cost, now = q.popleft()
-
-        for new_cost, new in graph[now]:
-            # 방문하지 않았다면
-            if not visited[new]:
-                # 더 작은 값으로 갱신, 다음 가중치도 더 작은 값
-                next_cost = min(now_cost, new_cost)
-                visited[new] = next_cost
-                q.append((next_cost, new))
-
-    # 정답 세기
     answer = 0
-    for i in range(1, N + 1):
-        # 자기자신이 아닐 때만
-        if i != start:
-            # USADO가 K보다 크거나 같다면 카운트
-            if visited[i] >= K:
+    while q:
+        now = q.popleft()
+
+        for cost, new in graph[now]:
+            # 방문하지 않고, 가중치가 K 이상이라면
+            if not visited[new] and cost >= K:
+                # 방문 처리 후 정답 세기
+                visited[new] = 1
                 answer += 1
+                q.append(new)
+
     return answer
 
 
 
 N, Q = map(int, input().split())
-# 그래프 연결 및 최댓값 저장
+# 그래프 연결
 graph = [[] for _ in range(N + 1)]
-maximum = 0
 for _ in range(N - 1):
     p, q, r = map(int, input().split())
     graph[p].append((r, q))
     graph[q].append((r, p))
-    # 최댓값 갱신
-    maximum = max(maximum, r)
+
 
 # K = k일 때, v를 보고 있는 소들에게 몇 개의 동영상이 추천?
 for _ in range(Q):
     k, v = map(int, input().split())
-    print(bfs(k, v, graph, maximum))
+    print(bfs(k, v, graph))
