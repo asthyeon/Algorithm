aa09155aa23ac7f23b01a9339ffdab05711b6ce0
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-01-07 19:28:43 +0900

    [Silver II] Title: 최소 힙, Time: 108 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -3,23 +3,30 @@ import sys
 input = sys.stdin.readline
 
 """
-# 최소 힙 (1927)
-1. 최소힙
+[문제] 최소 힙 (1927)
+1. 연산 규칙
+ - 배열에 자연수 x를 넣음
+ - x가 0일 때, 배열에서 가장 작은 값 출력 후, 그 값을 배열에서 제거
+
+[풀이]
+1. 우선순위 큐 이용
 """
 import heapq
 
-hq = []
-# 연산 정보
 N = int(input())
+hq = []
 for _ in range(N):
     x = int(input())
 
-    # 0 연산
+    # x가 0일 때
     if x == 0:
+        # 큐에 수가 있다면 출력
         if hq:
             print(heapq.heappop(hq))
+        # 큐에 수가 없다면 0 출력
         else:
             print(0)
-    # 그 외 연산
+    # x가 자연수 일때 인큐
     else:
         heapq.heappush(hq, x)
+
