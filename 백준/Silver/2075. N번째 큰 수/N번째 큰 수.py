723caf5b0c47dc21a1ff934f6a78e9249e534888
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-01-11 13:58:03 +0900

    [Silver III] Title: N번째 큰 수, Time: 2124 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -3,39 +3,31 @@ import sys
 input = sys.stdin.readline
 
 """
-# N번째 큰 수 (2075)
- 1. NxN 표에 채워진 수들은 자신의 한 칸 위에 있는 수보다 큼
-[입력]
- 1. N: 표의 크기
- 2. N개의 줄: N개의 수가 주어짐
-[출력]
- 1. N번째 큰 수 출력
-"""
+[문제] N번째 큰 수
+1. 표의 모든 수는 자신의 한 칸 위에 있는 수보다 큼
+2. N번째 큰 수를 찾기
 
-"""
-<풀이>
- 1. 우선순위큐
- 2. 전체를 돌리면 메모리 초과
- 3. 힙큐의 길이를 N개로 유지 -> N번째로 큰 숫자를 가장 작은 값으로
+[풀이]
+1. 우선순위 큐
+2. 모든 수를 하나의 리스트로 넣기 -> 메모리 초과
+3. 메모리를 N개 길이로 유지
 """
 import heapq
 
 N = int(input())
-
-# 우선순위큐
 hq = []
 for _ in range(N):
-    table = list(map(int, input().split()))
-    
-    for number in table:
-        # 힙큐의 길이가 N보다 작을 때는 인큐
+    row = list(map(int, input().split()))
+    for r in range(N):
+        now = row[r]
+        # hq 길이가 N개보다 짧다면 인큐
         if len(hq) < N:
-            heapq.heappush(hq, number)
-        # 힙큐의 길이가 N일 때
+            heapq.heappush(hq, now)
+        # hq 길이가 N개보다 같거나 크다면 제일 작은걸 빼기
         else:
-            # 가장 작은 값이 이번 숫자보다 작거나 같다면 교체
-            if hq[0] <= number:
-                heapq.heappop(hq)
-                heapq.heappush(hq, number)
+            new = heapq.heappop(hq)
+            heapq.heappush(hq, max(now, new))
 
-print(hq[0])
+# 정답 출력
+answer = heapq.heappop(hq)
+print(answer)
