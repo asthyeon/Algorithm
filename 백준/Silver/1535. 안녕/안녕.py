16cd8dfd17837fe8db81207ad27b7d0818deacd4
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-01-19 20:12:41 +0900

    [Silver II] Title: 안녕, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -3,52 +3,37 @@ import sys
 input = sys.stdin.readline
 
 """
-# 안녕 (1535)
- 1. 세준이를 생각해준 사람 1번 ~ N번
- 2. i번 사람에게 인사를 하면 L[i]만큼 체력 잃고, J[i]만큼 기쁨 얻음
- 3. 각각의 사람에게 최대 1번만 말할 수 있음
- 4. 세준이의 체력은 100, 0 이하가 될시 기쁨을 못 느낌
- 5. 주어진 체력 내에서 최대한의 기쁨 느끼기
-[입력]
- 1. N: 사람 수
- 2. 둘째 줄: 인사할 때 잃는 체력
- 3. 셋째 줄: 인사할 때 얻는 기쁨
-[출력]
- 1. 얻을 수 있는 최대 기쁨
-"""
+[문제] 안녕 (1535)
+1. 세준이가 i번 사람에게 인사를 하면
+ - 체력: -L[i]
+ - 기쁨: +J[i]
+2. 최대한의 기쁨을 얻는 방법?
+3. 체력이 0 혹은 음수가 되면 죽어서 기쁨은 0
 
-"""
-<풀이>
- 1. 일단 풀어보기 -> 백트래킹 -> 시간초과
- 2. dp
+[풀이]
+1. dp
+2. 인사를 할 수도 안할 수도 있음
+3. 체력을 기준으로 
 """
 
 
-# dp
-def dynamic_programming(N, healths, joys):
-    # 모든 체력의 경우 구하기
-    dp = [[0] * 101 for _ in range(N + 1)]
+def dynamic_programming(N, L, J):
+    # 체력 100 ~ 0
+    dp = [0] * 101
 
-    # 사람 수만큼 순회
-    for i in range(1, N + 1):
-        # 체력 순회
-        for j in range(1, 101):
-            # 이전 값으로 교체
-            dp[i][j] = dp[i - 1][j]
+    # 각 사람 순회
+    for i in range(N):
+        # 100부터 이번 사람 체력까지 역순으로
+        for l in range(100, L[i], -1):
+            # 현재 체력의 기쁨 vs 인사 후 체력의 기쁨
+            dp[l] = max(dp[l], dp[l - L[i]] + J[i])
 
-            # 만약 이번에 기쁨을 얻을 수 있다면
-            if healths[i] <= j:
-                # 현재 값과 기쁨을 얻었을 때 중 큰 값으로 교체
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - healths[i]] + joys[i])
-
-    return dp[N][99]
+    # 가장 큰 값
+    return max(dp)
 
 
-# 사람 수
 N = int(input())
-# 잃는 체력 리스트
-healths = [0] + list(map(int, input().split()))
-# 얻는 기쁨 리스트
-joys = [0] + list(map(int, input().split()))
+L = list(map(int, input().split()))
+J = list(map(int, input().split()))
 
-print(dynamic_programming(N, healths, joys))
+print(dynamic_programming(N, L, J))
