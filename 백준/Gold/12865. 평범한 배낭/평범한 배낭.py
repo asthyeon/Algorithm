e29fa357a2cbc782aa9da6ed90c724a012c6b724
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-01-20 00:35:42 +0900

    [Gold V] Title: 평범한 배낭, Time: 1180 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,44 +3,36 @@ import sys
 input = sys.stdin.readline
 
 """
-# 무게에 맞는 최대 가치 구하기
-@ 풀이
-(1) dp로 풀기
-(2) 2차원 배열로 무게 기준으로 풀기(가치를 최대한 담고, 담기전과 담은 후를 비교하기)
-    무게 0  1  2  3  4  5  6  7
-물품
-0        0  0  0  0  0  0  0  0
-6 13     0  0  0  0  0  0  13 13
-4 8      0  0  0  0  8  8  13 13
-3 6      0  0  0  6  8  8  13 14
-5 12     0  0  0  6  8  12 13 14
+[문제] 평범한 배낭 (12865)
+1. 각 물건은 무게 W와 가치 V를 가짐
+2. 해당 물건을 배낭에 넣어서 가면 V만큼 즐길 수 있음
+3. 최대 K만큼의 무게만을 넣을 수 있는 배낭만 들고 다닐 수 있음
+
+[풀이]
+1. dp
+2. 무게 기준
 """
 
 
-# dp 함수
-def dp(N, K, WV_list):
-    dp = [[0] * (K + 1) for _ in range(N + 1)]
+def dynamic_programming(bags):
+    # 최대 무게 K
+    dp = [0] * (K + 1)
 
-    for x in range(1, N + 1):
-        for y in range(1, K + 1):
-            # 무게가 물품보다 더 크다면(배낭에 공간이 남는다면)
-            if y >= WV_list[x][0]:
-                # 이전 물품을 넣었을 때와 이전에 물품을 안넣었을 때에 지금 물품을 넣는 것과 비교
-                dp[x][y] = max(dp[x - 1][y], dp[x - 1][y - WV_list[x][0]] + WV_list[x][1])
-            # 무게가 더 작다면(배낭에 공간이 없다면)
-            else:
-                # 이전 물품을 넣었을 때와 같음
-                dp[x][y] = dp[x - 1][y]
+    # 가방 순회
+    for w, v in bags:
+        # 제일 큰 무게부터 해당 물건 무게까지 순회
+        for i in range(K, w - 1, -1):
+            # 현재 가방 무게 가치 vs 해당 물건 더한 가치
+            dp[i] = max(dp[i], dp[i - w] + v)
+    
+    # 가방의 무게가 최대일 때 반환
+    return dp[K]
 
-    return dp[N][K]
 
-# 물품의 수: N, 버틸 수 있는 무게: K
 N, K = map(int, input().split())
-
-# 물건의 무게: W, 물건의 가치: V
-WV_list = [(0, 0)]
+bags = []
 for _ in range(N):
     W, V = map(int, input().split())
-    WV_list.append((W, V))
+    bags.append((W, V))
 
-print(dp(N, K, WV_list))
+print(dynamic_programming(bags))
