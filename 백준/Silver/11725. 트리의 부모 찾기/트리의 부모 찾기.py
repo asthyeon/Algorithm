0aa9089d4b6c466e27ac7b4a37f720403a42de15
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-02-08 15:37:19 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 248 ms, Memory: 51700 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -2,43 +2,45 @@ import sys
 # sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-
 """
-# 트리의 루트를 1이라고 할 때, 각 노드의 부모를 구하는 프로그램 작성
-@ 풀이
-(1) 일단 정점을 연결
-(2) DFS 사용
+[문제] 트리의 부모 찾기 (11725)
+1. 루트 없는 투리가 주어짐
+2. 트리의 루트를 1이라고 했을 때, 각 노드의 부모를 구하기
+
+[풀이]
+1. bfs -> 루트에서 탐색하기(이전 방문 노드가 부모)
 """
-sys.setrecursionlimit(10**9)
+from collections import deque
 
 
-# dfs 함수
-def dfs(n, adj, visited):
-    for i in adj[n]:
-        if visited[i] == 0:
-            visited[i] = n
-            dfs(i, adj, visited)
+def bfs(graph):
+    visited = [0] * (N + 1)
+    q = deque([1])
+    visited[1] = 1
+
+    while q:
+        now = q.popleft()
+        
+        # 다음 방문 노드 탐색
+        for new in graph[now]:
+            if not visited[new]:
+                # 다음 방문 노드의 부모 노드
+                visited[new] = now
+                q.append(new)
+
+    for i in range(2, N + 1):
+        print(visited[i])
 
 
-# 노드의 개수 N
 N = int(input())
-
-# 인접 리스트
-adj = [[] for _ in range(N + 1)]
-
-# 정점 연결하기
+# 그래프 연결
+graph = [[] for _ in range(N + 1)]
 for _ in range(N - 1):
-    n1, n2 = map(int, input().split())
+    a, b = map(int, input().split())
 
-    adj[n1].append(n2)
-    adj[n2].append(n1)
+    graph[a].append(b)
+    graph[b].append(a)
 
-# 방문 리스트
-visited = [0] * (N + 1)
+# 루트에서 탐색
+bfs(graph)
 
-# 함수 사용
-dfs(1, adj, visited)
-
-# 2번 노드부터 부모 노드 번호 출력
-for j in range(2, N + 1):
-    print(visited[j])
