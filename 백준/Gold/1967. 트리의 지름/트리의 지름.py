96094249de9b55ddbc3ef19bf057025b309ad18f
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-02-08 15:58:00 +0900

    [Gold IV] Title: 트리의 지름, Time: 4292 ms, Memory: 211848 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -3,67 +3,51 @@ import sys
 input = sys.stdin.readline
 
 """
-# 트리의 지름 (1967)
- 1. 트리에서 어떤 두 노드를 선택해 양쪽으로 쫙 당길 때 가장 길게 늘어나는 경우가 있음
-  - 트리의 모든 노드들은 이 두 노드를 지름의 끝 점으로 하는 원 안에 들어가게 됨
-  - 이런 두 노드 사이의 경로의 길이를 트리의 지름이라고 함
- 2. 트리에 존재하는 모든 경로들 중에서 가장 긴 것의 길이 = 트리의 지름
-[입력]
- 1. n: 노드의 개수
- 2. n - 1 개의 줄: 부모 노드, 자식 노드, 간선의 가중치
- 3. 루트의 노드 번호는 항상 1
-[출력]
- 1. 트리의 지름 출력
-"""
+[문제] 트리의 지름 (1967]
+1. 트리는 사이클이 없는 무방향 그래프
+2. 어떤 두 노드를 선택해도 둘 사이에 경로가 항상 하나만 존재
+3. 어떤 두 노드를 선택해서 양쪽으로 쫙 당길 때 가장 길게 늘어나는 경우 구하기
+4. 루트 노드는 1
+5. 간선에 대한 정보는 부모 노드의 번호가 작은 것이 먼저 입력됨
 
-"""
-<풀이>
- 1. bfs
+[풀이]
+1. 다익스트라
 """
 from collections import deque
 
 
-def bfs(tree, start):
-    # 가장 큰 지름과 노드 번호
-    diameter = 0
-    node = 0
-
-    # 끝 노드부터 시작
-    q = deque([(0, start)])
-    visited = [0] * (n + 1)
-    visited[start] = 1
+def dijkstra(tree, start):
+    distances = [10**9] * (n + 1)
+    q = deque([(start, 0)])
+    distances[start] = 0
 
     while q:
-        dist, now = q.popleft()
+        now, now_weight = q.popleft()
 
-        # 새로운 노드 탐색
-        for new_dist, new in tree[now]:
-            if not visited[new]:
-                visited[new] = 1
-                q.append((dist + new_dist, new))
+        # 다음 노드 탐색
+        for new, new_weight in tree[now]:
+            total_weight = now_weight + new_weight
+            # 다음 방문을 더 적은 값으로 방문한다면 방문
+            if distances[new] > total_weight:
+                q.append((new, total_weight))
+                distances[new] = total_weight
 
-                # 거리 갱신
-                if diameter < dist + new_dist:
-                    diameter = dist + new_dist
-                    node = new
-
-    return diameter, node
+    # 가장 긴 지름 반환
+    return max(distances[1:])
 
 
 n = int(input())
-# 트리 만들기
+# 트리 연결
 tree = [[] for _ in range(n + 1)]
 for _ in range(n - 1):
-    parent, child, weight = map(int, input().split())
+    a, b, c = map(int, input().split())
+    tree[a].append((b, c))
+    tree[b].append((a, c))
 
-    # 양방향
-    tree[child].append((weight, parent))
-    tree[parent].append((weight, child))
-
-# 루트 노드에서 가장 먼 노드 찾아 시작할 노드로 지정
-_, start_node = bfs(tree, 1)
-
-# 가장 먼 노드에서 지름 구하기
-answer, far_node = bfs(tree, start_node)
+# 모든 간선 간의 거리 구하기
+answer = 0
+for start in range(1, n + 1):
+    # 정답 교체
+    answer = max(answer, dijkstra(tree, start))
 
 print(answer)
