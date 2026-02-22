0d681640eb0bf97cc8840ccb0b0b8349d5022e7a
Taehyeon <139421094+asthyeon@users.noreply.github.com>
2026-02-23 01:16:54 +0900

    [Gold IV] Title: 타임머신, Time: 724 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -3,60 +3,62 @@ import sys
 input = sys.stdin.readline
 
 """
-# 1번 도시에서 출발해서 나머지 도시로 가는 가장 빠른 시간 순서대로 출력
-1. M 개의 버스가 A 에서 B 로 C 시간 만큼 걸림
-2. C = 0인 경우는 순간 이동, C < 0 인 경우 타임머신으로 시간을 되돌아감
-3. 1번 도시에서 어떤 도시로 갈 때 타임머신을 계속 타게 된다면 -1 출력
-4. 그렇지 않다면 가장 빠른 시간 순서대로 출력 및 경로 없는 도시는 -1 출력
-@ 풀이
-(1) 다익스트라 이용시 음수 값을 고려하지 못함
-(2) 벨만-포드 알고리즘 사용
+[문제] 타임머신 (11657)
+1. 순간이동 및 타임머신으로 시간을 되돌아가는 경우가 존재
+2. 1번 도시에서 출발해서 나머지 도시로 가는 가장 빠른 시간 출력
+3. 무한루프의 경우 -1만 출력, 해당 도시로 가는 경로가 없다면 -1 출력
+
+[풀이]
+1. 다익스트라 -> 음수 순환 불가
+2. 벨만 포드: 매 단계마다 모든 간선을 전부 확인하며 모든 노드 간의 최단 거리를 구함
 """
-import heapq
-INF = 999999999999
 
 
-# 다익스트라 함수
-def bellman_ford(buses, distances):
-    # 시작점 방문 처리
+def bellman_ford(graph):
+    # 거리 및 시작점 초기화
+    distances = [10e9] * (N + 1)
     distances[1] = 0
-    # N 번의 라운드 반복
+
+    # 도시 수만큼 반복
     for i in range(N):
-        # 매 라운드마다 모든 버스 노선 확인
+        # 버스 수만큼 반복
         for j in range(M):
-            # 현재지점, 다음지점, 거리
-            now = buses[j][0]
-            new = buses[j][1]
-            dist = buses[j][2]
-            # 현재지점이 도달 못하는 곳이 아닐 때
-            if distances[now] != INF:
-                # 다음지점이 현재 지점 거리값보다 크다면 갱신
-                if distances[new] > distances[now] + dist:
+            # 현재 위치, 다음 위치, 거리
+            now = graph[j][0]
+            new = graph[j][1]
+            dist = graph[j][2]
+            # 현재 위치에 도달할 수 있다면
+            if distances[now] < 10e9:
+                # 다음 위치로 더 짧게 방문할 수 있다면 갱신
+                if distances[now] + dist < distances[new]:
                     distances[new] = distances[now] + dist
-                    # N 번째 라운드에서도 값이 갱신된다면 음수 순환 존재
+                    # 마지막 라운드에서도 값이 갱신되는 경우 무한 루프로 판단
                     if i == N - 1:
-                        print(-1)
-                        return
+                        return -1
 
-    # 음수순환이 존재하지 않는다면
-    for k in range(2, N + 1):
-        # 도달할 수 없다면 -1 출력
-        if distances[k] == INF:
-            print(-1)
-        # 도달할 수 있다면 거리 출력
-        else:
-            print(distances[k])
+    # 음수 순환이 존재하지 않는 경우
+    return distances
 
 
-# 도시의 개수 N, 버스의 개수 M
+
 N, M = map(int, input().split())
-# 최단 거리 리스트
-distances = [INF] * (N + 1)
-# 모든 버스 노선에 대한 정보 리스트
-buses = []
+# 그래프 연결
+graph = []
 for _ in range(M):
-    # 시작 A, 도착 B, 거리 C
     A, B, C = map(int, input().split())
-    buses.append((A, B, C))
-    
-bellman_ford(buses, distances)
+    graph.append((A, B, C))
+
+answer = bellman_ford(graph)
+
+# 음수 순환인 경우
+if answer == -1:
+    print(answer)
+# 음수 순환이 아니라면
+else:
+    for i in range(2, N + 1):
+        # 도달할 수 없다면 -1
+        if answer[i] == 10e9:
+            print(-1)
+            continue
+        # 도달할 수 있다면 값 출력
+        print(answer[i])
